98f3dfe770a46fe37e916fcbfe75d315c2dd8738
Kyle F. Butts <buttskyle96@gmail.com>
2021-08-20 08:57:54 -0600

    Add xaringan printer

---
+++ R/xaringan_utils.R
@@ -0,0 +1,24 @@
+#' Print Xaringan .Rmd/.html file using `pagedown::chrome_print`
+#'
+#' @param file The full file path to .Rmd/.html file
+#' @param pdf_file Specify custom pdf output location. Otherwise the same
+#'   location of file will be used
+#'
+print_rmd <- function(file, pdf_file = NULL) {
+	if(is.null(pdf_file)) {
+		pdf_file = file
+		pdf_file = str_replace(pdf_file, ".Rmd", ".pdf")
+		pdf_file = str_replace(pdf_file, ".html", ".pdf")
+	}
+
+	# temp allows for easier error output
+	temp = NULL
+	try({
+		temp = pagedown::chrome_print(
+			file, pdf_file, wait = 10
+		)
+	})
+	if(is.null(temp)) {
+		cli::cli_alert_warning("{file} failed")
+	}
+}
