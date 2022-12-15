0bbbe2a05b87ca2272fa236dd7d6a1235f2f1eed
Kyle Butts <buttskyle96@gmail.com>
2022-12-15 10:28:35 -0700

    Copy R vector to clipboard for pasting

---
+++ R/helpful.R
@@ -0,0 +1,12 @@
+#' @title Prints vector with each item on a new line
+#' 
+#' @param vec Vector you want to print
+#' 
+#' @export
+copy_vec <- function(vec) {
+  vec |>
+    paste0(collapse = "\n") |>
+    clipr::write_clip()
+  cli::cli_alert_success("Copied to clipboard")
+}
+
