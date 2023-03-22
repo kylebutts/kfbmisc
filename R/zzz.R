a7c87a15addb95b881306bb88e424d1fa8a714bb
Kyle Butts <buttskyle96@gmail.com>
2023-03-22 15:26:00 -0600

    Update ggplot2 theme

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-	cli::cli_alert_info("Loading fonts for ggplot theme")
-	sysfonts::font_add_google("Fira Sans", "fira_sans")
-	sysfonts::font_add_google("Merriweather", "merriweather")
-	showtext::showtext_auto()
-}
