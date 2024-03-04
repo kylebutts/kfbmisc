6ee8a1464f088bd6fdb0174ce6a67efd3c6fe31d
Kyle Butts <buttskyle96@gmail.com>
2024-03-04 12:20:19 +0100

    `print_rmd` does not reknit `.html` file exists and is newer

--- R/print_rmd.R
+++ R/print_rmd.R
@@ -5,17 +5,38 @@
 #'   location of file will be used
 #'
 #' @export
-print_rmd <- function(file, pdf_file = NULL) {
+print_rmd <- function(file, pdf_file = NULL, verbose = TRUE) {
   if (is.null(pdf_file)) {
-    pdf_file <- paste0(xfun::with_ext(file, ".pdf"), ".pdf")
+    pdf_file <- fs::path_ext_set(file, ".pdf")
   }
-  
+  html_file <- fs::path_ext_set(file, ".html")
+  file <- "/Users/kylebutts/Documents/Mixtape-Sessions/Mixtape-Documents/Invoice.Rmd"
+
+  search = fs::dir_ls(
+    fs::path_dir(file), 
+    regexp = paste0(fs::path_ext_remove(file), "\\.[Rq]md")
+  )
+
+  Rmd_file = NULL
+  if (length(search) > 0) {
+    Rmd_file = search[1]
+  }
+
+  file_to_print <- file
+  if (
+    fs::file_exists(html_file) && 
+    fs::file_info(html_file)$modification_time > fs::file_info(Rmd_file)$modification_time
+  ) {
+    file_to_print = html_file
+  }
+
   # temp allows for easier error output
-  print("Attempting to print to pdf")
+  if (verbose) print("Attempting to print to pdf")
+  
   temp <- NULL
   try({
     temp <- pagedown::chrome_print(
-      file, pdf_file,
+      file_to_print, pdf_file,
       wait = 10
     )
   })
